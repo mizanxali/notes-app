31f16232f965f5cdd50f7bca890aed17d8bbb245
mizanxali <mizanalip786@gmail.com>
2020-04-13 23:54:26 +0530

    Final product commit

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView notesListView;
+    static ArrayList<String> notes = new ArrayList<String>();
+    static ArrayAdapter<String> arrayAdapter;
+    SharedPreferences preferences;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add) {
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notesListView = (ListView)findViewById(R.id.notesListView);
+        preferences = getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+
+
+        HashSet<String> presavedNotes = (HashSet<String>) preferences.getStringSet("notes", null);
+
+        if(presavedNotes == null) {
+            notes.add("Example Note : Start making notes!");
+        }
+        else {
+            notes = new ArrayList(presavedNotes);
+        }
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notes);
+        notesListView.setAdapter(arrayAdapter);
+
+        notesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+                intent.putExtra("NoteID", position);
+                startActivity(intent);
+            }
+        });
+
+        notesListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_menu_delete)
+                        .setTitle("Delete note?")
+                        .setMessage("Are you sure you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                notes.remove(position);
+                                arrayAdapter.notifyDataSetChanged();
+
+                                HashSet<String> set = new HashSet<String>(MainActivity.notes);
+                                preferences.edit().putStringSet("notes", set).apply();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
+            }
+        });
+    }
+}
